a857deb27efc48651bf47b7814e01fbbf18e0129
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-11-24 21:32:46 +0600

    added handleDeleteBuyer to delete a buyer form db

--- src/Pages/Admin/AllBuyers/AllBuyers.js
+++ src/Pages/Admin/AllBuyers/AllBuyers.js
@@ -1,12 +1,28 @@
 import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import toast from 'react-hot-toast';
 
 const AllBuyers = () => {
-    const { data: allBuyers = [] } = useQuery({
+    const { data: allBuyers = [], refetch } = useQuery({
         queryKey: ['/users/buyers'],
         queryFn: () => fetch('http://localhost:5000/users/buyers')
             .then(res => res.json())
     })
+
+    // handle delete buyer from db
+    const handleDeleteBuyer = buyer => {
+        const id = buyer._id;
+        fetch(`http://localhost:5000/users/buyers/${id}`, {
+            method: "DELETE",
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.deletedCount > 0) {
+                    toast.success(`${buyer.name} is deleted successfully`);
+                    refetch();
+                }
+            })
+    }
 
     return (
         <div>
@@ -29,7 +45,7 @@ const AllBuyers = () => {
                                     <td>{buyer.name}</td>
                                     <td>{buyer.email}</td>
                                     <td>
-                                        <button className='btn btn-sm bg-red-700 border-red-700 hover:bg-red-800'>Delete</button>
+                                        <button onClick={() => handleDeleteBuyer(buyer)} className='btn btn-sm bg-red-700 border-red-700 hover:bg-red-800'>Delete</button>
                                     </td>
                                 </tr>)
                         }
