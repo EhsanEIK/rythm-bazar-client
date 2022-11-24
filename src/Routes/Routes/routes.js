33489e23e5e6ec4993a311c9dcab655face3bb06
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-11-24 20:45:17 +0600

    added allBuyers route and page under admin panel

--- src/Routes/Routes/routes.js
+++ src/Routes/Routes/routes.js
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import AdminLayout from "../../layouts/AdminLayout";
 import Main from "../../layouts/Main";
 import Admin from "../../Pages/Admin/Admin/Admin";
+import AllBuyers from "../../Pages/Admin/AllBuyers/AllBuyers";
 import AllSellers from "../../Pages/Admin/AllSellers/AllSellers";
 import Home from "../../Pages/Home/Home/Home";
 import Login from "../../Pages/Login/Login";
@@ -52,6 +53,10 @@ const router = createBrowserRouter([
             {
                 path: '/admin/allSellers',
                 element: <AllSellers></AllSellers>,
+            },
+            {
+                path: '/admin/allBuyers',
+                element: <AllBuyers></AllBuyers>,
             },
         ]
     }
