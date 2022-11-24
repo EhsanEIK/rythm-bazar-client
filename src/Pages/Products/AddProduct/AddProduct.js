dee97f64eed4c3fcf18f183f40b33e145bc8cf15
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-11-24 18:39:19 +0600

    saved the product into the db from AddProduct page

--- src/Pages/Products/AddProduct/AddProduct.js
+++ src/Pages/Products/AddProduct/AddProduct.js
@@ -1,5 +1,7 @@
+import { useQuery } from '@tanstack/react-query';
 import React, { useContext } from 'react';
 import { useForm } from 'react-hook-form';
+import toast from 'react-hot-toast';
 import { AuthContext } from '../../../contexts/AuthProvider/AuthProvider';
 import useUserInfo from '../../../hooks/useUserInfo';
 
@@ -9,34 +11,67 @@ const AddProducts = () => {
 
     const { register, handleSubmit, formState: { errors }, } = useForm();
 
-    const handleAddProduct = data => {
+    const { data: categories = [] } = useQuery({
+        queryKey: ['categories'],
+        queryFn: async function () {
+            const res = await fetch('http://localhost:5000/categories');
+            const data = res.json();
+            return data;
+        }
+    })
+
+    const handleAddProduct = (data, event) => {
         const image = data.image[0];
         const formData = new FormData();
         formData.append('image', image);
 
+        // save the image into the imgbb site
         fetch(`https://api.imgbb.com/1/upload?key=${process.env.REACT_APP_ImgBB_API_Key}`, {
             method: "POST",
             body: formData,
         })
             .then(res => res.json())
-            .then(data => {
-                if (data.success) {
-                    const imageURL = data.data.url;
+            .then(imgData => {
+                if (imgData.success) {
+                    const date = new Date();
+                    const imageURL = imgData.data.url;
+                    const product = {
+                        sellerName: userInfo.name,
+                        email: user?.email,
+                        productName: data.productName,
+                        resalePrice: data.resalePrice,
+                        origianlPrice: data.originalPrice,
+                        yearsOfUse: data.yearsOfUse,
+                        category: data.category,
+                        image: imageURL,
+                        date: date + ''
+                    }
+
+                    // save the product details into the database
+                    fetch('http://localhost:5000/products', {
+                        method: "POST",
+                        headers: {
+                            'content-type': 'application/json'
+                        },
+                        body: JSON.stringify(product)
+                    })
+                        .then(res => res.json())
+                        .then(data => {
+                            if (data.acknowledged) {
+                                toast.success('Product added successfully');
+                                event.target.reset();
+                            }
+                        })
                 }
             })
-        // const product = {
-        //     productName: data.productName,
-        //     resalePrice: data.resalePrice,
-        //     origianlPrice: data.originalPrice,
-        //     yearsOfUse:data.yearsOfUse,
-        // }
     }
 
     return (
         <div className='my-10'>
-            <h1 className='text-5xl text-center font-semibold mb-8'>Add New Product</h1>
+            <h1 className='text-5xl text-center font-semibold mb-5'>Add New Product</h1>
             <div className='flex justify-center'>
-                <form onSubmit={handleSubmit(handleAddProduct)}>
+                <form onSubmit={handleSubmit(handleAddProduct)}
+                    className="border-1 rounded-sm shadow-lg p-10 w-[400px]">
                     <div className="form-control w-full">
                         <label className="label"><span className="label-text">Product Name</span></label>
                         <input type="text"
@@ -58,27 +93,42 @@ const AddProducts = () => {
                     <div className="form-control w-full">
                         <label className="label"><span className="label-text">Origianl Price</span></label>
                         <input type="text"
-                            {...register("origianlPrice", {
+                            {...register("originalPrice", {
                                 required: "Origianl Price is required"
                             })}
                             className="input input-bordered w-full" />
-                        {errors.origianlPrice && <p role="alert" className='text-red-600 mt-1'>{errors.origianlPrice?.message}</p>}
+                        {errors.originalPrice && <p role="alert" className='text-red-600 mt-1'>{errors.originalPrice?.message}</p>}
                     </div>
                     <div className="form-control w-full">
                         <label className="label"><span className="label-text">Years of Use</span></label>
                         <input type="text"
-                            {...register("yearsOfUser", {
+                            {...register("yearsOfUse", {
                                 required: "Years of use is required"
                             })}
                             className="input input-bordered w-full" />
-                        {errors.yearsOfUser && <p role="alert" className='text-red-600 mt-1'>{errors.yearsOfUser?.message}</p>}
+                        {errors.yearsOfUse && <p role="alert" className='text-red-600 mt-1'>{errors.yearsOfUse?.message}</p>}
+                    </div>
+                    <div className="form-control w-full">
+                        <label className="label"><span className="label-text">Select Category</span></label>
+                        <select {...register("category", {
+                            required: "Please select a category"
+                        })} className="select select-bordered w-full">
+                            <option>--- SELECT ---</option>
+                            {
+                                categories.map(category =>
+                                    <option
+                                        key={category._id}
+                                        value={category._id}>{category.name}</option>)
+                            }
+                            {errors.category && <p role="alert" className='text-red-600 mt-1'>{errors.category?.message}</p>}
+                        </select>
                     </div>
                     <div className="form-control w-full mt-5">
                         <input type="file"
                             {...register("image", {
                                 required: "Image is required"
                             })}
-                            accept='image/*' className="file-input file-input-sm file-input-bordered w-full" />
+                            accept='image/*' className="file-input file-input-md file-input-bordered w-full" />
                         {errors.image && <p role="alert" className='text-red-600 mt-1'>{errors.image?.message}</p>}
                     </div>
                     <button className="btn btn-md bg-teal-700 hover:bg-teal-800 text-white border-teal-700 w-full mt-5">Add Product</button>
