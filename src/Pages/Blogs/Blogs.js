702225a291ae538953f0f65d4cd9b351bdb68843
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-11-26 20:13:56 +0600

    added blog-02 in Blogs page

--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import prototypalInheriance from '../../assets/blogs/prototypal-inheritance.png';
 
 const Blogs = () => {
     return (
         <div>
             {/* blog-01 */}
-            <div className='bg-slate-100 rounded-lg p-10'>
+            <div className='bg-slate-100 rounded-lg p-10 mb-20'>
                 <h2 className='md:text-3xl text-lg font-bold mb-5'>1. What are the different ways to manage a state in a React application?</h2>
                 <p>There are four main types of state to properly manage the React apps:
                     <ol className='list-decimal ml-10'>
@@ -24,6 +25,15 @@ const Blogs = () => {
                     In many cases, a lot of major parts of our application rely upon accessing URL state. Try to imagine building a blog without being able to fetch a post based off of its slug or id that is located in the URL!
                     <br />
                 </p>
+            </div>
+            {/* blog-02 */}
+            <div className='bg-slate-100 rounded-lg p-10 mb-20'>
+                <h2 className='md:text-3xl text-lg font-bold mb-5'>2. How does prototypical inheritance work?</h2>
+                <p>The Prototypal Inheritance is a feature in javascript used to add methods and properties in objects. It is a method by which an object can inherit the properties and methods of another object. Traditionally, in order to get and set the [[Prototype]] of an object, we use Object.getPrototypeOf and Object.
+                    <br />
+                    When we read a property from object, and it’s missing, JavaScript automatically takes it from the prototype. In programming, this is called “prototypal inheritance”.
+                </p>
+                <img className='mt-5' src={prototypalInheriance} alt="prototypalInheriance" />
             </div>
         </div>
     );
