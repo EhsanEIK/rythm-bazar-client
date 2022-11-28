10177aa3eeec38250c5ccb8635f1206c44c0b47f
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-11-28 13:41:53 +0600

    updated Dashboard.js file

--- src/Pages/Dashboard/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard/Dashboard.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../../contexts/AuthProvider/AuthProvider';
+import useUserInfo from '../../../hooks/useUserInfo';
 
 const Dashboard = () => {
+    const { user } = useContext(AuthContext);
+    const [userInfo] = useUserInfo(user?.email);
+
     return (
         <div>
-            <h1 className='text-2xl font-semibold mb-14'>
-                Welcome To Dashboard
-            </h1>
+            <h1 className='text-3xl font-semibold mb-10'>Welcome To Dashboard</h1>
+            <h3 className='text-2xl font-semibold mb-3'>Your Info</h3>
+            <p><b className='mr-2'>Name:</b>{userInfo.name}</p>
+            <p><b className='mr-3'>Email:</b>{userInfo.email}</p>
+            <p><b className='mr-5'>Role:</b>{userInfo.userRole}</p>
         </div>
     );
 };
